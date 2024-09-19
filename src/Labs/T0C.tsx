fc87789b7b145351e0cfacb43e3c0525a9af889f
Tao_22 <66737948+Jason-Tao22@users.noreply.github.com>
2024-09-18 23:47:16 -0700

    changes

--- src/Labs/T0C.tsx
+++ src/Labs/T0C.tsx
@@ -4,16 +4,24 @@ export default function TOC() {
   return (
     <ul>
       <li>
-        <Link to="/Labs">Labs</Link>
+        <Link to="https://github.com/Jason-Tao22/kanbas-react-web-app/tree/main/src/Labs">
+          Labs
+        </Link>
       </li>
       <li>
-        <Link to="/Labs/Lab1">Lab 1</Link>
+        <Link to="https://github.com/Jason-Tao22/kanbas-react-web-app/tree/main/src/Labs/Lab1">
+          Lab 1
+        </Link>
       </li>
       <li>
-        <Link to="/Labs/Lab2">Lab 2</Link>
+        <Link to="https://github.com/Jason-Tao22/kanbas-react-web-app/tree/main/src/Labs/Lab2">
+          Lab 2
+        </Link>
       </li>
       <li>
-        <Link to="/Labs/Lab3">Lab 3</Link>
+        <Link to="https://github.com/Jason-Tao22/kanbas-react-web-app/tree/main/src/Labs/Lab3">
+          Lab 3
+        </Link>
       </li>
       <li>
         <Link to="/Kanbas">Kanbas</Link>

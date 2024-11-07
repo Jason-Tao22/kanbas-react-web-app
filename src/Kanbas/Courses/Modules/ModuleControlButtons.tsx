853fc4c1e3fe4ff8af0af34f4f8f475380230fd3
Tao_22 <66737948+Jason-Tao22@users.noreply.github.com>
2024-11-06 17:30:57 -0800

    update

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -1,13 +1,35 @@
-import { IoEllipsisVertical } from "react-icons/io5";
 import GreenCheckmark from "./GreenCheckmark";
-import { BsPlus } from "react-icons/bs";
+import { FaPlus, FaPencil } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-export default function LessonControlButtons() {
+export default function LessonControlButtons({
+  moduleId,
+  deleteModule,
+  editModule,
+}: {
+  moduleId: string;
+  deleteModule: (moduleId: string) => void;
+  editModule: (moduleId: string) => void;
+}) {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+
   return (
     <div className="float-end">
-      <GreenCheckmark />
-      <BsPlus className="fs-3" />
-      <IoEllipsisVertical className="fs-4" />
+      {currentUser.role === "FACULTY" && (
+        <>
+          <FaPencil
+            onClick={() => editModule(moduleId)}
+            className="text-primary me-3"
+          />
+          <FaTrash
+            className="text-danger me-2 mb-1"
+            onClick={() => deleteModule(moduleId)}
+          />
+          <GreenCheckmark />
+          <FaPlus className="fs-5" />
+        </>
+      )}
     </div>
   );
 }
